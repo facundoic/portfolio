e19eb65884d5acad3fd0ffeceaa9cdc21a0cf521
facundoic <facundoceballos1994@hotmail.com>
2021-12-01 19:45:18 -0300

    changes on css file

--- PortfolioGithub/resources/views/home/base.blade.php
+++ PortfolioGithub/resources/views/home/base.blade.php
@@ -5,21 +5,63 @@
         <title>Portfolio</title>
     </head>
     <body>
-        <div class="parallax-mirror">
-            <header class="header">
-                <nav class="header-nav">
-                    <h3 class="activate-nav-list">Menu</h3>
-                    <ul class="nav-list">
-                        <li><a href="/proyectos">Proyectos</a></li>
-                        <li><a href="/conocimientos">Conocimientos</a></li>
-                        <li><a href="/contacto">Contacto</a></li>
-                    </ul>
-                </nav>
-            </header>
+        <div class="parallax">
+            <img class="picture-parallax" src="/images/fondo2.jpg">
+        </div>
+        <!--- header  start
+             --------------------------- -->
+        <header class="header-s">
+            <nav class="nav-header">
+                <ul>
+                    <li class="menu">Menu
+                        <ul class="nav-list">
+                            <li><a href="/proyectos">Proyectos</a></li>
+                            <li><a href="/conocimientos">Conocimientos</a></li>
+                            <li><a href="/contacto">Contacto</a></li>
+                        </ul>
+                    </li>
+                </ul>    
+            </nav>
+        </header>
+        <!--- header  end
+             --------------------------- -->
+             
+        <!--- section home start
+             --------------------------- -->
+        <section id="home">
+            <div class="container">
+                <div id="ball"></div>
+                @yield("contenido")
+            </div>
+        </section>
+         <!--- section home end
+             --------------------------- -->
+         <!--- section proyectos start
+             --------------------------- -->
+        <section id="proyectos">
+            <div class="container">
 
-        <div class="container">
-            @yield("contenido")
-        </div>
-        </div>
+            </div>
+        </section>
+         <!--- section proyectos end
+             --------------------------- -->
+         <!--- section conocimientos start
+             --------------------------- -->
+        <section id="conocimientos">
+            <div class="container">
+
+            </div>
+        </section>
+         <!--- section conocimientos end
+             --------------------------- -->
+        <!--- section contacto start
+             --------------------------- -->
+        <section id="contacto">
+            <div class="container">
+
+            </div>
+        </section>
+        <!--- section contacto end
+             --------------------------- -->
     </body>
 </html>
