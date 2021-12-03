6b3971a8f52646fc4ab3488729dbffa36134c0e9
facundoic <facundoceballos1994@hotmail.com>
2021-12-03 16:43:54 -0300

    new changes 3 dic

--- PortfolioGithub/resources/views/home/base.blade.php
+++ PortfolioGithub/resources/views/home/base.blade.php
@@ -5,20 +5,44 @@
         <title>Portfolio</title>
     </head>
     <body>
-        <div class="parallax" id="first">
-            <div>
-                <h2>First</h2>
-            </div>
+        <div class="container">
+            
+            <section class="home" id="section_home">
+                <div class="parallax">
+                    <header class="headerFixed">
+                        <nav>
+                            <a href="#section_home">Home</a>
+                            <a href="#section_proyectos">Proyectos</a>
+                            <a href="#section_conocimientos"">Conocimientos</a>
+                            <a href="#section_contacto">Contacto</a>
+                        </nav>
+                    </header>
+                <br><br>
+                <div class="content">
+                    <h2>This is home</h2>
+                </div>
+                </div>
+            </section>
+            
+
+            <section class="proyectos" id="section_proyectos">
+                <h1>This is proyectos</h1>
+            </section>
+
+            <section class="conocimientos" id="section_conocimientos">
+                <h1>This is conocimientos</h1>
+            </section>
+            
+            <section class="contacto" id="section_contacto">
+                <div class="parallax">
+                    <br><br>
+                <div class="content">
+                    <h2>This is Contacto</h2>
+                </div>
+                </div>
+            </section>
+
         </div>
-        <div id="mid">
-            <h2>Mid</h2>
-        </div>
-        <div class="parallax" id="end">
-            <div>
-                <h2>End</h2>
-            </div>
-        </div>
-        
-        <script src="{{ asset('js/base.js') }}"></script>
+
     </body>
 </html>
